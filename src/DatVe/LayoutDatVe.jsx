a2cabb5230fd4ef006b2044567e7ebf95abcebff
Nghia-Truong <chuon.chongia1306@gmail.com>
2024-10-28 22:14:02 +0900

    update1

---
+++ src/DatVe/LayoutDatVe.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListGheNgoi from "./ListGheNgoi";
+
+export default function LayoutDatVe() {
+    return (
+        <div>
+            <ListGheNgoi />
+        </div>
+    )
+}
